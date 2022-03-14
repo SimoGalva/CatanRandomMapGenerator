0fd312ccecaf3c73b7385c5b42e745648ff35ce5
simop <simo.piaf@gmail.com>
2022-03-14 14:42:43 +0100

    mix: aggiustamenti minori e documentazione

--- src/main/java/engine/MapGeneratorEngine.java
+++ src/main/java/engine/MapGeneratorEngine.java
@@ -4,7 +4,9 @@ import coordinate.HexagonalCoordinate4PHandler;
 import hexagon.CentralHexagon;
 import hexagon.HexagonPoint;
 import hexagon.HexagonalBase;
+import hexagon.material.MaterialHandler;
 import hexagon.material.Materials;
+import hexagon.number.NumberHandler;
 import hexagon.number.Numbers;
 import island.IslandController;
 
@@ -14,6 +16,9 @@ public class MapGeneratorEngine {
     private final Logger logger = Logger.getLogger(getClass().getName());
 
     private HexagonalCoordinate4PHandler coordinateHandler;
+    private MaterialHandler materialHandler;
+    private NumberHandler numberHandler;
+
     private static final String LAND = "LAND";
     private static final String LAND_NO_RIVER = "LAND_NO_R";
     private static final String LAND_SEA = "LAND_SEA";
@@ -39,8 +44,8 @@ public class MapGeneratorEngine {
     public void generateIsland (IslandController controller) {
         HexagonPoint islandCntr = controller.getIslandHexCenter();
         int pointerCntrDim = coordinateHandler.calculatePointerDimesnsion(islandCntr);
-        Materials materialCntr = MaterialHandler.pickRandomMaterial("LAND");
-        Numbers numberCntr = NumberHandler.pickRandomNumber(materialCntr);
+        Materials materialCntr = materialHandler.pickRandomMaterial("LAND");
+        Numbers numberCntr = numberHandler.pickRandomNumber(materialCntr);
         HexagonalBase  cntrHex = new CentralHexagon(materialCntr, numberCntr, pointerCntrDim, islandCntr);
         //todo: riprendi da qui
     }
@@ -51,6 +56,8 @@ public class MapGeneratorEngine {
 
     private MapGeneratorEngine() {
         this.coordinateHandler = new HexagonalCoordinate4PHandler();
+        this.materialHandler = new MaterialHandler();
+        this.numberHandler = new NumberHandler();
     }
 
     public static final MapGeneratorEngine getInstance() {
